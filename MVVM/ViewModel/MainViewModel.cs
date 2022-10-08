d1de7bf5d60ab0f34921e7fce9aa98420a925186
Muadz Izh <muadzihharul@gmail.com>
2022-10-08 18:49:06 +0300

    Add project files.

---
+++ MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using storeManagement.Core;
+
+namespace storeManagement.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand TransactionViewCommand { get; set; }
+
+        public HomeViewModel HomeVM { get; set; }
+
+        public TransactionViewModel TransactionVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { 
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            TransactionVM = new TransactionViewModel();
+
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+            
+            TransactionViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = TransactionVM;
+            });
+
+        }
+
+    }
+}
