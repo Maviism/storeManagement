db1f8a6eb7e1bdaa413f5c6c1f97c0bf190e11e4
Muadz Izh <muadzihharul@gmail.com>
2022-11-26 20:33:42 +0300

    Refactor : uninstall useless nugget depedencies

--- Core/ObservableObject.cs
+++ Core/ObservableObject.cs
@@ -5,24 +5,12 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
-using LiveChartsCore;
 
 namespace storeManagement.Core
 {
     class ObservableObject : INotifyPropertyChanged
     {
-        private ISeries[] _series;
 
-        public ISeries[] Series
-        {
-            get => _series;
-            set
-            {
-                if (_series == value) return;
-                _series = value;
-                OnPropertyChanged(nameof(Series));
-            }
-        }
         public event PropertyChangedEventHandler? PropertyChanged;
 
         protected void OnPropertyChanged([CallerMemberName] string name = null) 
